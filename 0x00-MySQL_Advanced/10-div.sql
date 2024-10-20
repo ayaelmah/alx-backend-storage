7f24306a6a650231a8b1d43b481f8b9e89b621df
ayaelmah <ayaelmahraoui22@gmail.com>
2024-10-20 21:56:06 +0000

    MySQL

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- Makes sure that devisions are safe
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+		RETURN (IF (b = 0, 0, a / b));
+	END //
+
+	DELIMITER ;
---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,4 @@
+-- Decreasing item quantity after adding a new one
+
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
